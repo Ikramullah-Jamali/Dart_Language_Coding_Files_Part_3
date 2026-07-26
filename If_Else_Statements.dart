69a487353e23851afc31d8ba8969bf9f87a264a8
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-07-26 14:07:05 +0500

    Add files via upload

---
+++ If_Else_Statements.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  print("--- If Else Statements ---");
+   int marks=45;
+   if(marks>50)
+     {
+       print("Result: Pass");
+     }
+   else
+     {
+       print("Result: Fail");
+     }
+   print("Program End.");
+}
