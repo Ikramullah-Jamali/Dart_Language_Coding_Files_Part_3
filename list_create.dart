57e42aaa921e64ef3826894c2bc936137fc67fc0
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-08-02 17:03:51 +0500

    Add files via upload

---
+++ list_create.dart
@@ -0,0 +1,14 @@
+// Creating List
+void main()
+{
+  // fixed values List
+  List<int> numbers=[1,2,3];
+  print(numbers);
+  // Empty list
+  List<String> names=[];
+  print(names);
+  // Filled List
+  List<int> filled=List.filled(3,0);
+  print(filled);
+
+}
