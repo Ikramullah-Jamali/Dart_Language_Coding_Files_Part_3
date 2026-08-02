4e6a203759d51a0fecc43118f11ad90a404e39b6
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-08-02 17:04:10 +0500

    Add files via upload

---
+++ list_generics.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  // List of integers -> type-safe
+  List<int> numbers=[1,2,3];
+  numbers.add(4); // add element
+  print(numbers);
+
+  // List of Strings
+  List<String> names=["ali","ahmed"];
+  names.add("sara");
+  print(names);
+}
