b366d87fb2fc6cda37d35d88284ef74ba64808f2
Ikramullah-Jamali <ikramullahjamali12@gmail.com>
2026-08-02 17:04:59 +0500

    Add files via upload

---
+++ list_properties.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  List<int> nums=[0,1,2,3];
+
+  print(nums.length); // Print total Elements
+  print(nums.first); // Print first Element
+  print(nums.last); // Print Last Element
+}
